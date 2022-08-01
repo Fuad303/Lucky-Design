e1b53eaefaf4a4227711f27108d49c666068617c
Fuad303 <sadiqovfuad8@gmail.com>
2022-08-01 11:12:59 +0400

    some possibly meaningful message

--- cart.js
+++ cart.js
@@ -52,8 +52,6 @@
 
   // Top Function  
 var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
